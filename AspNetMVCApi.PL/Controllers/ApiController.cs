586a8882f329f9521521e8518ce5d56a44afd35c
sgorkemcil <sgorkemcil@gmail.com>
2022-05-17 14:57:24 +0300

    BLL katmanında StudentService oluşturuldu. StudentController PL katmanına eklendi. update-database ile veri tabanı oluşturuldu. Yarın Aspnt MVC için DI uygulanacaktır ve apinin metotları kontrol edilecektir.

---
+++ AspNetMVCApi.PL/Controllers/ApiController.cs
@@ -0,0 +1,51 @@
+﻿using AspNetMVCApi_BLL.ContractBLL;
+using AspNetMVCApi_EL.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace AspNetMVCApi_PL.Controllers
+{
+    public class StudentController : ApiController
+    {
+        private readonly IStudentService _studentService;
+
+        public StudentController(IStudentService studentService)
+        {
+            _studentService = studentService;
+        }
+
+
+        // GET api/<controller>
+        public ResponseData GetAllStudents()
+        {
+            try
+            {
+                var result = _studentService.GetAllStudents();
+                return new ResponseData() { IsSuccess = true, Data = result };
+            }
+            catch (Exception ex)
+            {
+                // ex loglanabilir
+                return new ResponseData()
+                {
+                    IsSuccess = false,
+                    Message = ex.Message
+                };
+            }
+        }
+
+
+        // GET api/<controller>/5
+
+        // POST api/<controller>
+
+        // PUT api/<controller>/5
+
+        // DELETE api/<controller>/5
+    }
+}
---
+++ AspNetMVCApi_BLL/ContractBLL/IStudentService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetMVCApi_EL.ViewModels;
+namespace AspNetMVCApi_BLL.ContractBLL
+{
+    public interface IStudentService
+    {
+        ResponseData GetAllStudents();
+        ResponseData AddStudet(StudentVM student);
+
+        ResponseData UpdateStudent(int studentId,string newName,string newSurname);
+
+        ResponseData DeleteStudent(int studentId);
+
+    }
+}
